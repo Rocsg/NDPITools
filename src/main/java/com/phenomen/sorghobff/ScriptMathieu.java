a9339dd34e58ad40d6eea5196578970fdec5f7e9
Rocsg <romainfernandez06@gmail.com>
2021-04-28 15:12:13 +0200

    Tracking #1

--- src/main/java/com/phenomen/sorghobff/ScriptMathieu.java
+++ src/main/java/com/phenomen/sorghobff/ScriptMathieu.java
@@ -1,7 +1,6 @@
-package com.phenomen.sorghobff;
+package com.phenomen.factory;
 
 import com.phenomen.common.VitimageUtils;
-import com.phenomen.mlutils.SegmentationUtils;
 
 import ij.IJ;
 import ij.ImageJ;
@@ -9,29 +8,51 @@ import ij.ImagePlus;
 
 public class ScriptMathieu {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ImageJ im = new ImageJ();
-		
-		//faireTest0();
-		faireTest1();
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ImageJ im = new ImageJ();
 
-	public static void faireTest0() {		
-		System.out.println("Blabla");
-	}
-	
-	public static void faireTest1() {		
-		ImagePlus impRef = IJ.openImage("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/FromRomain/To_Mat/Weka_test/Stack_annotations_512_pix.tif");
-		ImagePlus impTest = IJ.openImage("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/FromRomain/To_Mat/Marc_test/Stack_annotations_512_pix.tif");
-	
-		impRef.show();
-		impTest.show();
-		
-		IJ.log(""+SegmentationUtils.IOU(impRef, impTest));
-		SegmentationUtils.scoreComparisonSegmentations(impRef, impTest);
-		
-		VitimageUtils.waitFor(10000);
-		System.exit(0);
-	}
+        //faireTest0();
+        interexpertTest();
+
+    }
+
+    public static void faireTest0() {        
+        System.out.println("Blabla");
+    }
+
+    public static void faireTest1() {        
+        ImagePlus impRef = IJ.openImage("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/FromRomain/To_Mat/Weka_test/Stack_annotations_512_pix.tif");
+        ImagePlus impTest = IJ.openImage("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/FromRomain/To_Mat/Marc_test/Stack_annotations_512_pix.tif");
+
+        impRef.show();
+        impTest.show();
+
+        IJ.log(""+SegmentationUtils.IOU(impRef, impTest));
+        SegmentationUtils.scoreComparisonSegmentations(impRef, impTest);
+
+        VitimageUtils.waitFor(10000);
+        System.exit(0);
+    }
+
+    public static void interexpertTest() {
+        ImagePlus[] imgMatthieu=SegmentationUtils.jsonToBinary("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/InterExpert/Interexpert_assessment/Interexpert_Mathieu_subsampled_512_pix");
+        imgMatthieu[1].show();
+        ImagePlus[] imgRomain=SegmentationUtils.jsonToBinary("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/InterExpert/Interexpert_assessment/Interexpert_Romain_subsampled_512_pix");
+        imgRomain[1].show();
+
+        ImagePlus imgBase = imgMatthieu[0];
+        ImagePlus impRef = imgRomain[1];
+        ImagePlus impTest = imgMatthieu[1];
+
+        SegmentationUtils.scoreComparisonSegmentations(impRef, impTest);
+        ImagePlus result1 = SegmentationUtils.visualizeMaskDifferenceOnSourceData(imgBase,impRef,impTest);
+        //ImagePlus result2 = SegmentationUtils.visualizeMaskEffectOnSourceData(impRef,impTest,0);
+
+        result1.show();
+        //result2.show();
+
+        VitimageUtils.waitFor(5000);
+
+    }
 }
