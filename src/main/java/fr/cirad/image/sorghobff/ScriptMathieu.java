52ededf88b3d97980199f9b5626aab89ad2092bd
Rocsg <romainfernandez06@gmail.com>
2021-05-05 14:47:52 +0200

    Update package names

---
+++ src/main/java/fr/cirad/image/sorghobff/ScriptMathieu.java
@@ -0,0 +1,63 @@
+package fr.cirad.image.sorghobff;
+
+import fr.cirad.image.common.VitimageUtils;
+
+import fr.cirad.image.mlutils.SegmentationUtils;
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+
+public class ScriptMathieu {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ImageJ im = new ImageJ();
+
+        //faireTest0();
+        interexpertTest();
+
+    }
+
+    public static void faireTest0() {        
+        System.out.println("Blabla");
+    }
+
+    public static void maCame() {
+    	System.out.println("Test merge");
+    }
+    
+    public static void faireTest1() {        
+        ImagePlus impRef = IJ.openImage("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/FromRomain/To_Mat/Weka_test/Stack_annotations_512_pix.tif");
+        ImagePlus impTest = IJ.openImage("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/FromRomain/To_Mat/Marc_test/Stack_annotations_512_pix.tif");
+
+        impRef.show();
+        impTest.show();
+
+        IJ.log(""+SegmentationUtils.IOU(impRef, impTest));
+        SegmentationUtils.scoreComparisonSegmentations(impRef, impTest);
+
+        VitimageUtils.waitFor(10000);
+        System.exit(0);
+    }
+
+    public static void interexpertTest() {
+        ImagePlus[] imgMatthieu=SegmentationUtils.jsonToBinary("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/InterExpert/Interexpert_assessment/Interexpert_Mathieu_subsampled_512_pix");
+        imgMatthieu[1].show();
+        ImagePlus[] imgRomain=SegmentationUtils.jsonToBinary("E:/DONNEES/Matthieu/Projet_VaisseauxSorgho/InterExpert/Interexpert_assessment/Interexpert_Romain_subsampled_512_pix");
+        imgRomain[1].show();
+
+        ImagePlus imgBase = imgMatthieu[0];
+        ImagePlus impRef = imgRomain[1];
+        ImagePlus impTest = imgMatthieu[1];
+
+        SegmentationUtils.scoreComparisonSegmentations(impRef, impTest);
+        ImagePlus result1 = SegmentationUtils.visualizeMaskDifferenceOnSourceData(imgBase,impRef,impTest);
+        //ImagePlus result2 = SegmentationUtils.visualizeMaskEffectOnSourceData(impRef,impTest,0);
+
+        result1.show();
+        //result2.show();
+
+        VitimageUtils.waitFor(5000);
+
+    }
+}
